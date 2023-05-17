d04791f86e3acee56116ab2fe3cc6a18d5055a60
yusixian <3450414733@qq.com>
2023-05-17 21:34:06 +0800

    fix: music  tag set

--- src/controller/music.controller.ts
+++ src/controller/music.controller.ts
@@ -53,13 +53,13 @@ class MusicController {
   public async updateMusic(req: Request, res: Response) {
     const id = req.params.id;
     const [e, updatedMusic] = await silentHandle(() => {
-      const { tags, ...rest } = req.body;
-      const tagsArgs = tags.map((tag: { name: string }) => ({ create: tag, where: { name: tag.name } }));
+      const { tagNames, ...rest } = req.body;
+      const tagsArgs = tagNames.map((tagName: string) => ({ name: tagName }));
       console.log({ tagsArgs });
       musicService.updateMusicById(parseInt(id), {
         ...rest,
         tags: {
-          connectOrCreate: tagsArgs,
+          set: tagsArgs,
         },
       });
     });
